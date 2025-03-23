122381055b9a8e39acce838215422ba1ef350fa2
berhocode <153773550+berhocode@users.noreply.github.com>
2025-03-23 12:06:31 +0000

    Update qr_code.py
    fix some problems, improve the code

--- qr_code.py
+++ qr_code.py
@@ -1,18 +1,29 @@
 import qrcode
 import qrcode.constants
 
-qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L) #box_size=20, border=10
-print('hello, welcome to our QRcode generator APP!')
-user_data = input("> ")
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L)
+
+print('Hello, welcome to our QR Code Generator APP!')
+user_data = input("> ").strip()
 qr.add_data(user_data)
 qr.make(fit=True)
+
 available_colors = ["red", "blue", "white", "pink", "green"]
-user_color = input("what's the background color you prefer?  (red, blue, white, pink, green)> ")
-if user_color not in available_colors:
-    print("Invailable color, pls try again")
-else:
-    print(f"{user_color} applied!")
-data_name = input("Enter the name of your QRcode img > ")
-img = qr.make_image(back_color=user_color)
+user_color = input("What's the background color you prefer? (red, blue, white, pink, green) > ").strip().lower()
+
+while user_color not in available_colors:
+    print("Invalid color, please try again.")
+    user_color = input("Choose a valid background color: (red, blue, white, pink, green) > ").strip().lower()
+
+print(f"{user_color} applied!")
+
+# Allow user to choose QR color
+qr_color = input("Choose the QR code color (black, white, blue, red, green) > ").strip().lower()
+
+data_name = input("Enter the name of your QR code image > ").strip()
+
+# Generate and save the QR code
+img = qr.make_image(fill_color=qr_color, back_color=user_color)
 img.save(f"{data_name}.png")
- 
+
+print(f"QR Code saved as {data_name}.png!")
